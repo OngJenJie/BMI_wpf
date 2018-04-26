b38e71747489d8f4197de45415356349ed37039d
CancerNeko <jenjieong@yahoo.com>
2018-04-26 17:53:18 +0800

    加了slider 還有計算方式

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -43,8 +43,8 @@ namespace WpfApplication1
 
         private void btn_calculate_Click(object sender, RoutedEventArgs e)
         {
-            double height = int.Parse(txtbox_height.Text)/100;
-            double weight = int.Parse(txtboc_weight.Text);
+            double height = double.Parse(txtbox_height.Text)/100;
+            double weight = double.Parse(txtboc_weight.Text);
             double bmi = weight / (height * height);
 
             txtbox_result.Text =bmi.ToString() ;
@@ -53,11 +53,34 @@ namespace WpfApplication1
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("恭喜你！要繼續保持下去哦！");
+            
         }
 
         private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("這樣不行哦！好好努力減肥吧！");
+        }
+
+        private void slider_height_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtbox_height.Text = slider_height.Value.ToString();
+
+            double height = double.Parse(txtbox_height.Text) / 100;
+            double weight = double.Parse(txtboc_weight.Text);
+            double bmi = weight / (height * height);
+
+            txtbox_result.Text = bmi.ToString();
+        }
+
+        private void slider_weight_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtboc_weight.Text = slider_weight.Value.ToString();
+
+            double height = double.Parse(txtbox_height.Text) / 100;
+            double weight = double.Parse(txtboc_weight.Text);
+            double bmi = weight / (height * height);
+
+            txtbox_result.Text = bmi.ToString();
         }
     }
 }
